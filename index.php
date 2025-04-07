d8a253a677535e646342b4e54c95b8cbb1b03b4b
nbjafar <abujafaralfarhad@gmail.com>
2025-04-07 16:39:14 +0200

    first commit...

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<title>PizzaCornerMetsakulma</title>
+<style>
+  body { text-align: center; padding: 150px; }
+  h1 { font-size: 50px; }
+  body { font: 20px Helvetica, sans-serif; color: #333; }
+  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+  a { color: #dc8100; text-decoration: none; }
+  a:hover { color: #333; text-decoration: none; }
+</style>
+<article>
+    <h1>Pizza Corner Metsakulma</h1>
+    <div>
+        <p>We are open everyday to serve you great food. We are ready to deliver your favorite food at your doorstep. You can order your food by calling us or just place the order in whatsapp!</p>
+        <p>&mdash; For order call +358466367424 or whatsapp</p>
+	<p>&mdash; PizzaCornerMetsakulma</p>
+
+<!--
+<a aria-label="Chat on WhatsApp" href="https://wa.me/358466367424"> <img alt="Chat on WhatsApp" src="chatonwhatsapp.png" />
+-->
+<a aria-label="Order on WhatsApp" href="https://wa.me/358466367424"> <img alt="Order on WhatsApp" src="orderonwhatsapp.png" />
+<a />
+<br>
+<br>
+
+<div class="container" style="text-align: center; background-color: yellow; padding: 10px;">
+    <h2 style="font-weight: bold;">10% discount on WhatsApp order</h2>
+</div>
+    </div>
+</article>
+<br>
+<br>
+<!-- Histats.com  (div with counter) --><div id="histats_counter"></div>
+<!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,4942229,4,2050,280,25,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4942229&101" alt="" border="0"></a></noscript>
+<!-- Histats.com  END  -->
